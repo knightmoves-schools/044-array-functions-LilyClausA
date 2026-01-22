6caf4826294e1dcc4cd36a8854eeb78c7a951166
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-22 20:13:17 +0000

    zdefn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let automobiles = ['car', 'truck', 'motocycle'];
+let automobiles = ['car', 'truck', 'motorcycle'];
 automobiles.push('airplane', 'skateboard');
 automobiles.unshift('bike', 'helicopter');
 let automobilesCopy = `{automobiles}`
